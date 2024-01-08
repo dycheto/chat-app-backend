21cb69d2edcd7008cd51a12774ec0df9b785af94
Tihomir Dukov <t.i.dukov@gmail.com>
2024-01-08 12:17:10 +0200

    User entity done

---
+++ src/main/java/com/dycheto/chatapp/entity/User.java
@@ -0,0 +1,57 @@
+package com.dycheto.chatapp.entity;
+
+import jakarta.persistence.*;
+import org.springframework.beans.factory.annotation.Value;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
